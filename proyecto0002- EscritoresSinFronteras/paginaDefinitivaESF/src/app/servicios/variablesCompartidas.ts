282af10cc066628861c75112e99be9b67641c866
KenVeraChan <kenveracha@gmail.com>
2025-12-09 00:22:20 +0100

    Casi terminada la interfaz de HISTORIA

--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/servicios/variablesCompartidas.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/servicios/variablesCompartidas.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class VariablesCompartidas {
+
+    //AREA NOTICIAS DEL APARTADO "INICIO"
       public noticia:Noticias[]=[
         new Noticias(    
         "Titular de la noticia",
@@ -32,6 +34,14 @@ export class VariablesCompartidas {
           )
     ];
 
+      //AREA HISTORIAS DEL APARTADO "NUESTRA HISTORIA"
+      public historias: Historias[]=[
+        new Historias(9,"2024","Inicio en un grupo de escritura y lectura online"),
+        new Historias(12,"2024","Escritores Sin Fronteras, crea su primer logo corporativo"),
+        new Historias(1,"2025","Escritores Sin Fronteras, crece en usuarios suscritos tanto de Facebook como de Instagram"),
+        new Historias(5,"2025","Escritores Sin Fronteras, consigue su registro mercantil en Colombia como corporación online en existencia"),
+        new Historias(8,"2025","Escritores Sin Fronteras, registra sus primeras ganancias")];
+
       public escenas:string[]=[
         "Noticia 1: Nuevo concurso literario abierto a todos los escritores emergentes.",
         "Noticia 2: Entrevista exclusiva con el autor best-seller del año.",
@@ -59,4 +69,41 @@ class Noticias{
     public getNoticia():string{
         return this.noticia;
     } 
+}
+class Historias
+{
+    private mes: number=0; //Luego se convertirá en el nombre del mes según numero incluido
+    private anio:string="";
+    private contenido: string="";
+
+    constructor(mes: number,anio: string,contenido:string)
+    {
+      this.mes=mes;
+      this.anio=anio;
+      this.contenido=contenido;
+    }
+    public setAnio(anio:string):void
+    {
+      this.anio=anio
+    }
+    public setMes(mes:number):void
+    {
+      this.mes=mes;
+    }
+    public setContenido(contenido:string):void
+    {
+      this.contenido=contenido;
+    }
+    public getAnio():string
+    {
+      return this.anio;
+    }
+    public getMes():number
+    {
+      return this.mes;
+    }
+    public getContenido():string
+    {
+      return this.contenido;
+    }
 }
