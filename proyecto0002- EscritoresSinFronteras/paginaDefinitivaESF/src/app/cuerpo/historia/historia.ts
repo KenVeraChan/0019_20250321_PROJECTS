33cba67bd6b52d26df6e140e98456d32103aeab7
KenVeraChan <kenveracha@gmail.com>
2025-12-09 11:51:45 +0100

    Nuevos avances en interfaz historia - falta el redimensionado

--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/historia/historia.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/historia/historia.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, QueryList, ViewChildren, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, QueryList,ViewChild, ViewChildren, Renderer2, ElementRef } from '@angular/core';
 import { VariablesCompartidas } from '../../servicios/variablesCompartidas';
 
 @Component({
@@ -9,10 +9,12 @@ import { VariablesCompartidas } from '../../servicios/variablesCompartidas';
 })
 export class Historia implements OnInit, AfterViewInit {
   public tamanioHorizontalPantalla:number=0.0;
+  public punteroAvance:number=0.0;
   ngOnInit()
   {
     this.tamanioHorizontalPantalla = window.innerWidth;  //Ancho de la pantalla
-  }    
+  }
+  @ViewChild('fondoHistoria') fondoHistoria!:ElementRef;  
   @ViewChildren('botones') botones!: QueryList<ElementRef>;   //ELEMENTOS DEL DOM PARA MOVERSE (múltiples)
   constructor(private renderer: Renderer2) {}
   public objetoHistorias= new VariablesCompartidas();
@@ -26,27 +28,30 @@ export class Historia implements OnInit, AfterViewInit {
     if (typeof window !== 'undefined') 
     {     
       // Iterar sobre todos los botones (QueryList)
+      const fondos= this.fondoHistoria.nativeElement as HTMLElement;
       this.botones.forEach((item: ElementRef, index: number) => {
-            const botonesAccionados = item.nativeElement as HTMLElement;
-            this.renderer.setStyle(botonesAccionados, 'display', 'block');
-            // Añadir delay progresivo: cada botón se mueve después del anterior (100ms * índice)
-            const delay = index * 100; // 0ms, 100ms, 200ms, 300ms, etc.
-            
-            setTimeout(() => {
-              requestAnimationFrame(() => {
-                this.renderer.setStyle(botonesAccionados, 'transform', `translate(10px,${70 +index*70}px)`);
-                this.renderer.setStyle(botonesAccionados, 'transition', 'transform 0.5s ease-in-out');
-              });
-            }, delay);
+        const botonesAccionados = item.nativeElement as HTMLElement;
+        this.renderer.setStyle(botonesAccionados, 'display', 'block');
+        // Añadir delay progresivo: cada botón se mueve después del anterior (100ms * índice)
+        const delay = index * 100; // 0ms, 100ms, 200ms, 300ms, etc.
+        
+        setTimeout(() => {
+          requestAnimationFrame(() => {
+            this.renderer.setStyle(botonesAccionados, 'transform', `translate(10px,${70 +index*70}px)`);
+            this.renderer.setStyle(botonesAccionados, 'transition', 'transform 0.5s ease-in-out');
+          });
+        }, delay);
+        this.punteroAvance=70+(index+1)*80;   //Contabilización de la longitud para estirarse y cubrir todas las fechas
       });
+      this.renderer.setStyle(fondos, 'height', this.punteroAvance+'px'); //Se estira la longitud hasta cubrir todas las fechas encontradas
     }
   }
   public cerrarHistorial():void
   {
     if (typeof window !== 'undefined') 
     {     
-      // Iterar sobre todos los botones (QueryList)
       this.botones.forEach((item: ElementRef, index: number) => {
+            const fondos= this.fondoHistoria.nativeElement as HTMLElement;
             const botonesAccionados = item.nativeElement as HTMLElement;
             // Añadir delay progresivo: cada botón se mueve después del anterior (100ms * índice)
             const delay = index * 100; // 0ms, 100ms, 200ms, 300ms, etc.
@@ -57,6 +62,7 @@ export class Historia implements OnInit, AfterViewInit {
               });
             }, delay);
           this.renderer.setStyle(botonesAccionados, 'display', 'none');
+          this.renderer.setStyle(fondos, 'height', '300px');  //Se devuelve a la longitud inicial del FONDO HISTORIA
       });
     }
   }
