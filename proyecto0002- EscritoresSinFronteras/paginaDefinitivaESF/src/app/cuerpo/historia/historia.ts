175cc8067600d9409c0e636c1b03b0263315b503
KenVeraChan <kenveracha@gmail.com>
2025-12-11 13:32:45 +0100

    Terminada zona de historias, falta rellenar historias y redimensionar para resto pantallas

--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/historia/historia.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/historia/historia.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, QueryList,ViewChild, ViewChildren, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, QueryList,ViewChild, ViewChildren, Renderer2, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { VariablesCompartidas } from '../../servicios/variablesCompartidas';
 
 @Component({
@@ -10,94 +10,55 @@ import { VariablesCompartidas } from '../../servicios/variablesCompartidas';
 export class Historia implements OnInit, AfterViewInit {
   public tamanioHorizontalPantalla:number=0.0;
   public punteroAvance:number=0.0;
-  public eleccion:number=0.0;
+  public eleccion:number=0;
   public aniosNumero:number[]=[];     //Para el vector de años para exponer
   public mesesTexto:string[]=[];      //Para el vector de meses para exponer
   public objetoHistorias= new VariablesCompartidas();
-  constructor(private renderer: Renderer2) {}
-
+  constructor(private renderer: Renderer2,private deteccionCambio: ChangeDetectorRef) {}
+        //Renderer2 permite modificar elementos DOM y ChangeDetectorRef pemirte detectar cambios en variables
   @ViewChild('fondoHistoria') fondoHistoria!:ElementRef;  
   @ViewChildren('botones') botones!: QueryList<ElementRef>;   //ELEMENTOS DEL DOM PARA MOVERSE (múltiples)
+  @ViewChildren('botonesPrincipales') botonesPrincipales!:QueryList<ElementRef>;   //BOTONES PRINCIPALES DEL MENU
 
   ngOnInit()
   {
     this.tamanioHorizontalPantalla = window.innerWidth;  //Ancho de la pantalla
-    this.eleccion=1;   //Se inicia con la primera opción
   }
   ngAfterViewInit(): void {
     // Llamar aquí cuando los elementos del DOM ya existen
   }
 
-public cargarHistorial(opcion:number):void
-{
-    switch(opcion)
-    {
-      case 1:
-        {
-          //MUESTRA TODO SIN RESTRICCION ALGUNA
-            if (typeof window !== 'undefined') 
-            {   
-            this.eleccion=1; //Se elige la primera opción
-            }
-          break;
-        }
-      case 2:
-        {
-          //SE CARGARÁN TODOS LOS AÑOS Y SE MOSTRARÁ AL USUARIO
-          //AL HACER CLIC SOBRE UN AÑO SE MOSTRARÁN TODOS LOS MESES DE ESE AÑO
-          if (typeof window !== 'undefined') 
-            {   
-                this.eleccion=2; //Se elige la segunda opción
-              if (typeof window !== 'undefined') 
-              {
-                for(let i=0;i<this.objetoHistorias.historias.length;i++)
-                {
-                  this.aniosNumero[i]=Number(this.objetoHistorias.historias[i].getAnio());
-                }
-              }
-            }
-          break;
-        }
-      case 3:
-        {
-          //SE CARGARÁN TODOS LOS MESES DEL AÑO Y SE MOSTRARÁ AL USUARIO
-          //AL HACER CLIC SOBRE UN MES CONCRETO, SE MOSTRARÁN TODOS LOS MESES SEÑALADOS DE TODOS LOS AÑOS
-          if (typeof window !== 'undefined') 
-            {   
-              this.eleccion=3; //Se elige la tercera opción
-              if (typeof window !== 'undefined') 
-              {
-                for(let i=0;i<this.objetoHistorias.historias.length;i++)
-                {
-                  this.mesesTexto[i]=this.objetoHistorias.historias[i].getMes();
-                }
-              }
-            }
-          break;
-        }
-      default:
-        {
-          //No hace nada
-          if (typeof window !== 'undefined'){}
-          break;
-        }
+  public cargarHistorial(opcion:number):void
+  {
+    if (typeof window !== 'undefined') 
+    {   
+     this.eleccion=opcion; //Se elige la primera opción
+     this.deteccionCambio.detectChanges(); // fuerza actualización inmediata
+     
+      const fondos= this.fondoHistoria.nativeElement as HTMLElement;
+      this.botones.forEach((item: ElementRef, index: number) => {
+        const botonesAccionados = item.nativeElement as HTMLElement;
+        this.renderer.setStyle(botonesAccionados, 'display','block');
+        // Añadir delay progresivo: cada botón se mueve después del anterior (100ms * índice)
+        const delay = index * 100; // 0ms, 100ms, 200ms, 300ms, etc.
+        setTimeout(() => {
+          requestAnimationFrame(() => {
+            this.renderer.setStyle(botonesAccionados, 'transform', `translate(10px,${70 +index*70}px)`);
+            this.renderer.setStyle(botonesAccionados, 'transition', 'transform 0.5s ease-in-out');
+          });
+        }, delay);
+        this.punteroAvance=70+(index+1)*80;   //Contabilización de la longitud para estirarse y cubrir todas las fechas
+        //this.renderer.setStyle(botonesPrincipales, 'display','none');   //Se hacen desaparecer
+
+      });
+      this.renderer.setStyle(fondos, 'height', this.punteroAvance+'px'); //Se estira la longitud hasta cubrir todas las fechas encontradas  
+      
+      //Botones principales del menu de opciones de la historia
+      this.botonesPrincipales.forEach((item: ElementRef) => {
+            const botonesPrincipales= item.nativeElement as HTMLElement;   //Para hacerlos desaparecer mientras aparecen los otros
+            this.renderer.setStyle(botonesPrincipales,'display','none');
+      });
     }
-    // Iterar sobre todos los botones (QueryList)
-    const fondos= this.fondoHistoria.nativeElement as HTMLElement;
-    this.botones.forEach((item: ElementRef, index: number) => {
-      const botonesAccionados = item.nativeElement as HTMLElement;
-      this.renderer.setStyle(botonesAccionados, 'display', 'block');
-      // Añadir delay progresivo: cada botón se mueve después del anterior (100ms * índice)
-      const delay = index * 100; // 0ms, 100ms, 200ms, 300ms, etc.
-      setTimeout(() => {
-        requestAnimationFrame(() => {
-          this.renderer.setStyle(botonesAccionados, 'transform', `translate(10px,${70 +index*70}px)`);
-          this.renderer.setStyle(botonesAccionados, 'transition', 'transform 0.5s ease-in-out');
-        });
-      }, delay);
-      this.punteroAvance=70+(index+1)*80;   //Contabilización de la longitud para estirarse y cubrir todas las fechas
-    });
-    this.renderer.setStyle(fondos, 'height', this.punteroAvance+'px'); //Se estira la longitud hasta cubrir todas las fechas encontradas
   }
   public cerrarHistorial():void
   {
@@ -114,8 +75,13 @@ public cargarHistorial(opcion:number):void
                 this.renderer.setStyle(botonesAccionados, 'transition', 'transform 0.5s ease-in-out');
               });
             }, delay);
-          this.renderer.setStyle(botonesAccionados, 'display', 'none');
+          this.renderer.setStyle(botonesAccionados, 'display','none');
           this.renderer.setStyle(fondos, 'height', '300px');  //Se devuelve a la longitud inicial del FONDO HISTORIA
+      });
+      //Botones principales del menu de opciones de la historia
+      this.botonesPrincipales.forEach((item: ElementRef) => {
+            const botonesPrincipales= item.nativeElement as HTMLElement;   //Para hacerlos desaparecer mientras aparecen los otros
+            this.renderer.setStyle(botonesPrincipales,'display','block');
       });
     }
   }
