f80d1282951f7510f31d55c372b95a09a8380013
KenVeraChan <kenveracha@gmail.com>
2025-12-21 22:53:19 +0100

    Mejoras en novela COMETIDO 4321

--- proyecto0001 - Novela/0000_MenuPrincipal/javascriptpagina.js
+++ proyecto0001 - Novela/0000_MenuPrincipal/javascriptpagina.js
@@ -1,39 +1,145 @@
-const matrizCelda= document.getElementsByClassName("celda");
 const matrizCaja= document.getElementsByClassName("caja");
 const matrizCajaOculta= document.getElementsByClassName("cajaOculta");
 const matrizFila= document.getElementById("tablaContenidos");
-const pulsador= document.getElementsByClassName("enlaceActivo");    
+const pulsador= document.getElementsByClassName("enlaceActivo"); 
+let enlace= document.createElement("a");   
+let anchoPantalla=0;
 
-function activador()
+function cargando()
 {
     //proceso de aparición de la tabla
         matrizFila.style.transitionDuration = "0.5s";
-        matrizFila.style.marginLeft = "20%"
-    //proceso eventual de cambio de cartel
-    for(let puntero=0;puntero<matrizCaja.length;puntero++)
-        {
-            matrizCelda[puntero].addEventListener("mouseenter", function(){
-                matrizCaja[puntero].style.transitionDuration = "0.25s";
-                matrizCaja[puntero].style.transform = "rotate3d(1,0,0,90deg)";
-                //SE DARA LA VUELTA A LA CARA OCULTA MIENTRAS SE MUESTRA LA CARA PRINCIPAL
-                matrizCajaOculta[puntero].style.transitionDelay = "0.25s";
-                matrizCajaOculta[puntero].style.transitionDuration = "0.25s";
-                matrizCajaOculta[puntero].style.transform = "rotate3d(1,0,0,0deg)";
-                matrizCajaOculta[puntero].style.zIndex = "0";
-            });
-            matrizCelda[puntero].addEventListener("mouseleave", function(){
-                //SE DARA LA VUELTA A LA CARA OCULTA PARA DEJARLA EN OCULTA DE NUEVO
-
-                matrizCajaOculta[puntero].style.transitionDuration = "0.25s";
-                matrizCajaOculta[puntero].style.transform = "rotate3d(1,0,0,90deg)";
-                matrizCajaOculta[puntero].style.zIndex = "-1";
-
-                matrizCajaOculta[puntero].style.transitionDelay = "0.25s";
-                matrizCaja[puntero].style.transitionDuration = "0.25s";
-                matrizCaja[puntero].style.transform = "rotate3d(1,0,0,0deg)";
-            });
-            pulsador[puntero].addEventListener("click", function(){
-                
-            });
-        }
+        matrizFila.style.marginLeft = "10%";
+        anchoPantalla=window.innerWidth;
 }
+
+function activador(puntero)
+{
+    //proceso eventual de cambio de cartel
+        matrizCaja[puntero].addEventListener("mouseenter", function(){
+        matrizCaja[puntero].style.transitionDuration = "0.15s";
+        matrizCaja[puntero].style.transform = "rotate3d(1,0,0,360deg)";
+        matrizCaja[puntero].style.backgroundColor= "rgba(18, 158, 139, 1)";
+        matrizCaja[puntero].textContent="";
+        matrizCaja[puntero].appendChild(enlace);
+            anchoPantalla=window.innerWidth;  //Recoge el valor del ancho de la pantalla
+        if(anchoPantalla>=100 && anchoPantalla<=200)
+        {
+            enlace.setAttribute('style','font-size:x-small; color:white; text-decoration:none; padding-left:8px');
+        }
+        if(anchoPantalla>200 && anchoPantalla<=550)
+        {
+            enlace.setAttribute('style','font-size:small; color:white; text-decoration:none; padding-left:8px');
+        }
+        if(anchoPantalla>500)
+        {
+            enlace.setAttribute('style','font-size:medium; color:white; text-decoration:none; padding-left:8px');
+        }
+            switch(puntero)
+            {
+                case 0:
+                    {
+                        enlace.setAttribute('href','../0001_Argumento/argumento.php');
+                        enlace.textContent="ACCEDER A ARGUMENTO"; 
+                        break;
+                    }
+                case 1:
+                    {
+                        enlace.setAttribute('href','../0002_Personajes/personajes.php');
+                        enlace.textContent="ACCEDER A PERSONAJES";
+                        break;
+                    }
+                case 2:
+                    {
+                        enlace.setAttribute('href','../0003_Cartografia/cartografia.php');
+                        enlace.textContent="ACCEDER A CARTOGRAFÍA";
+                        break;
+                    }
+                case 3:
+                    {
+                        enlace.setAttribute('href','../0004_Acontecimientos/acontecimientos.php');
+                        enlace.textContent="ACCEDER A ACONTECIMIENTOS";
+                        break;
+                    }
+                case 4:
+                    {
+                        enlace.setAttribute('href','../0005_Anotaciones/bloquesNovela.php');
+                        enlace.textContent="ACCEDER A BLOQUES";
+                        break;
+                    }
+                case 5:
+                    {
+                        enlace.setAttribute('href','../0006_Calendarios/calendariosPlanetarios.php');
+                        enlace.textContent="ACCEDER A CALENDARIOS";
+                        break;
+                    }
+                default:
+                    {   break;    }
+            }
+            //SE DARA LA VUELTA A LA CARA OCULTA MIENTRAS SE MUESTRA LA CARA PRINCIPAL
+        });
+    }
+
+function reactivador(puntero)
+{   
+        matrizCaja[puntero].addEventListener("mouseleave", function(){
+            //SE DARA LA VUELTA A LA CARA OCULTA PARA DEJARLA EN OCULTA DE NUEVO
+            matrizCaja[puntero].style.transitionDuration = "0.15s";
+            matrizCaja[puntero].style.transform = "rotate3d(1,0,0,360deg)";
+            matrizCaja[puntero].style.backgroundColor= "rgb(208, 255, 0)";
+            matrizCaja[puntero].style.color="black";
+            matrizCaja[puntero].style.paddingLeft="5px";
+            matrizCaja[puntero].style.fontSize="large";
+            anchoPantalla=window.innerWidth;  //Recoge el valor del ancho de la pantalla
+                if(anchoPantalla>=100 && anchoPantalla<=200)
+                {
+                    matrizCaja[puntero].style.fontSize="13px";
+                }
+                if(anchoPantalla>200 && anchoPantalla<=550)
+                {
+                    matrizCaja[puntero].style.fontSize="medium";
+                }
+                if(anchoPantalla>500)
+                {
+                    matrizCaja[puntero].style.fontSize="large";
+                }
+            switch(puntero)
+            {
+                case 0:
+                    {
+                        matrizCaja[puntero].textContent="ARGUMENTO";
+                        break;
+                    }
+                case 1:
+                    {
+                        matrizCaja[puntero].textContent="PERSONAJES";
+                        break;
+                    }
+                case 2:
+                    {
+                        matrizCaja[puntero].textContent="CARTOGRAFÍA";
+                        break;
+                    }
+                case 3:
+                    {
+                        matrizCaja[puntero].textContent="ACONTECIMIENTOS";
+                        break;
+                    }
+                case 4:
+                    {
+                        matrizCaja[puntero].textContent="BLOQUES";
+                        break;
+                    }
+                case 5:
+                    {
+                        matrizCaja[puntero].textContent="CALENDARIOS";
+                        break;
+                    }
+                default:
+                    {   break;    }
+            }
+        });
+        pulsador[puntero].addEventListener("click", function(){  
+    });
+}
+
--- proyecto0001 - Novela/0000_MenuPrincipal/paginaPrincipal.php
+++ proyecto0001 - Novela/0000_MenuPrincipal/paginaPrincipal.php
@@ -3,68 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu de la pagina</title>
+    <title>NOVELA COMETIDO 4321</title>
     <link rel="shortcut icon" href="/images/iconoPestania.ico" type="image/x-icon">
     <link rel="stylesheet" href="estilosPagina.css">
 </head>
-<body onload="activador()">
-<img id="imagenFondo" src="../0000_MenuPrincipal/images/cubos blancos.png" alt="planeta Tierra de fondo">
-    <div id="titulo">MENU PRINCIPAL</div>
-    <table id="tablaContenidos">
-       <tr>
-        <td class="celda">
-            <div class="caja">
-              ARGUMENTO
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0001_Argumento/argumento.php">ACCEDER A ARGUMENTO</a>
-            </div>
-        </td>
-        <td class="celda">
-            <div class="caja">
-              PERSONAJES
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0002_Personajes/personajes.php">ACCEDER A PERSONAJES</a>
-            </div>
-        </td>
-        <td class="celda">
-            <div class="caja">
-              CARTOGRAFÍA
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0003_Cartografia/cartografia.php">ACCEDER A CARTOGRAFÍA</a>
-            </div>
-        </td>
-       </tr>
-       <tr>
-        <td class="celda">
-            <div class="caja">
-              ACONTECIMIENTOS
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0004_Acontecimientos/acontecimientos.php">ACCEDER A ACONTECIMIENTOS</a>
-            </div>
-        </td>
-        <td class="celda">
-            <div class="caja">
-              ANOTACIONES
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0005_Anotaciones/anotaciones.php">ACCEDER A ANOTACIONES</a>
-            </div>
-        </td>
-        <td class="celda">
-            <div class="caja">
-              CALENDARIOS
-            </div>
-            <div class="cajaOculta">
-              <a class="enlaceActivo" href="../0006_Calendarios/calendariosPlanetarios.php">ACCEDER A CALENDARIOS</a>
-            </div>
-        </td>
-       </tr> 
+<body onload="cargando()">
 
-    </table>
+<header>
+    <div id="titulo">COMETIDO 4321</div>
+</header>
+<section>
+  <img id="imagenFondo" src="../0000_MenuPrincipal/images/cubos blancos.png" alt="planeta Tierra de fondo">
+      <div id="tablaContenidos">
+            <div class="caja" onmouseenter="activador(0)" onmouseleave="reactivador(0)">ARGUMENTO</div>
+            <div class="caja" onmouseenter="activador(1)" onmouseleave="reactivador(1)">PERSONAJES</div>
+            <div class="caja" onmouseenter="activador(2)" onmouseleave="reactivador(2)">CARTOGRAFÍA</div>
+            <div class="caja" onmouseenter="activador(3)" onmouseleave="reactivador(3)">ACONTECIMIENTOS</div>
+            <div class="caja" onmouseenter="activador(4)" onmouseleave="reactivador(4)">BLOQUES</div>
+            <div class="caja" onmouseenter="activador(5)" onmouseleave="reactivador(5)">CALENDARIOS</div>
+      </div>
+</section>
+<footer>
+
+</footer>
+
     <script src="javascriptpagina.js"></script>
 </body>
 </html>
