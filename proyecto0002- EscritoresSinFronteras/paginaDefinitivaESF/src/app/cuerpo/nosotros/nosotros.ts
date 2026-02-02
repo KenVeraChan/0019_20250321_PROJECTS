e8919266ada91be96ae6590f60ac1946e03c4b1a
KenVeraChan <kenveracha@gmail.com>
2026-02-03 00:59:24 +0100

    Sección equipo en curso - fase 2 - imagenes cargadas (Luego desde la BBDD)

--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/nosotros/nosotros.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/cuerpo/nosotros/nosotros.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VariablesCompartidas } from '../../servicios/variablesCompartidas';
 
 @Component({
   selector: 'app-nosotros',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './nosotros.css',
 })
 export class Nosotros {
-
+  public equipo= new VariablesCompartidas();
+  
 }
--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/servicios/variablesCompartidas.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/servicios/variablesCompartidas.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class VariablesCompartidas {
-
-    //AREA NOTICIAS DEL APARTADO "INICIO"
+      //AREA NOTICIAS DEL APARTADO "INICIO"
       public noticia:Noticias[]=[
         new Noticias(    
         "Titular de la noticia",
@@ -58,7 +57,6 @@ export class VariablesCompartidas {
               "Contenido detallado del décimo noticia que proporciona información relevante sobre el tema tratado."
           )
     ];
-
       //AREA HISTORIAS DEL APARTADO "NUESTRA HISTORIA"
       public historias: Historias[]=[
         new Historias(0,9,"2005","Inicio en un grupo de escritura y lectura online"),
@@ -85,7 +83,17 @@ export class VariablesCompartidas {
         new Historias(2,1,"2025","Escritores Sin Fronteras, crece en usuarios suscritos tanto de Facebook como de Instagram"),
         new Historias(3,5,"2025","Escritores Sin Fronteras, consigue su registro mercantil en Colombia como corporación online en existencia"),
         new Historias(4,8,"2025","Escritores Sin Fronteras, registra sus primeras ganancias")];
-
+      //AREA DE QUIENES SOMOS PARA EL APARTADO DE NOSOTROS
+      public quienesSomos:QuienesSomos[]=[
+        new QuienesSomos("Ken","Vera Chan","Español","Escritor y Fundador","Ken Vera Chan es un escritor apasionado por la literatura contemporánea y la narrativa innovadora. Con una carrera que abarca más de una década, Ken ha publicado varias novelas y colecciones de cuentos que han sido aclamadas por la crítica. Además de su trabajo literario, Ken es el fundador de 'Escritores Sin Fronteras', una plataforma dedicada a apoyar a escritores emergentes de todo el mundo.","assets/images/equipoESF/kenVeraChan.png"),
+        new QuienesSomos("Diana","Emilce Zamora","Colombiana","Editora y Co-Fundadora","Diana Emilce Zamora es una editora experimentada con un profundo amor por la literatura hispanoamericana. Ha trabajado en diversas editoriales, ayudando a dar forma a las voces de nuevos escritores. Como co-fundadora de 'Escritores Sin Fronteras', Diana se dedica a crear oportunidades para que los escritores de diferentes culturas puedan compartir sus historias con una audiencia global.","assets/images/equipoESF/dianaEmilceZamora.png"),
+        new QuienesSomos("Cassandra","Romanova","Italiana","Community Manager","Cassandra Romanova es una experta en gestión de comunidades en línea y redes sociales. Con una sólida experiencia en marketing digital, Cassandra ha ayudado a numerosas organizaciones a construir y mantener comunidades vibrantes. En 'Escritores Sin Fronteras', Cassandra se encarga de conectar a escritores y lectores, fomentando un espacio inclusivo para el intercambio de ideas y creatividad.","assets/images/equipoESF/cassandraRomanova.png"),
+        new QuienesSomos("Sergio","Alvear","Peruano","Desarrollador Web","Sergio Alvear es un desarrollador web talentoso con una pasión por crear experiencias digitales intuitivas y atractivas. Con experiencia en diversas tecnologías web, Sergio ha contribuido al desarrollo de múltiples plataformas en línea. En 'Escritores Sin Fronteras', Sergio es responsable de mantener y mejorar la infraestructura técnica del sitio, asegurando que los usuarios tengan una experiencia fluida y agradable.","assets/images/equipoESF/sergioAlvear.png"),
+        new QuienesSomos("Maria Isabel","Muñoz","Colombiana","Diseñadora Gráfica","Maria Isabel Muñoz es una diseñadora gráfica creativa con un ojo para el detalle y la estética visual. Ha trabajado en proyectos de diseño para editoriales, campañas publicitarias y plataformas digitales. En 'Escritores Sin Fronteras', Maria Isabel se encarga de la identidad visual de la plataforma, creando diseños que reflejan la misión y los valores de la comunidad de escritores.","assets/images/equipoESF/mariaIsabelMunioz.png"),
+        new QuienesSomos("Daniela","Patrone","Argentina","Coordinadora de Eventos","Daniela Patrone es una profesional en gestión de eventos con una pasión por la literatura y la cultura. Ha organizado numerosos eventos literarios, talleres y conferencias que han reunido a escritores y lectores de diversas partes del mundo. En 'Escritores Sin Fronteras', Daniela coordina eventos que promueven la interacción y el crecimiento de la comunidad literaria.","assets/images/equipoESF/danielaPatrone.png"),
+        new QuienesSomos("Mario Alberto","Gómez","Argentino","Diseño gráfico y multimedia","Mario Alberto Gómez es un diseñador gráfico y multimedia con una amplia experiencia en la creación de contenido visual para plataformas digitales. Ha trabajado en proyectos que van desde el diseño web hasta la producción de videos promocionales. En 'Escritores Sin Fronteras', Mario Alberto aporta su talento para desarrollar materiales visuales que enriquecen la experiencia de los usuarios y promueven la plataforma.","assets/images/equipoESF/marioAlbertoGomez.png")
+      ];
+      //AREA DE ESCENAS PARA EL APARTADO DE SERVICIOS
       public escenas:string[]=[
         "Noticia 1: Nuevo concurso literario abierto a todos los escritores emergentes.",
         "Noticia 2: Entrevista exclusiva con el autor best-seller del año.",
@@ -229,5 +237,70 @@ class Historias
     public getContenido():string
     {
       return this.contenido;
+    }
+}
+class QuienesSomos{
+  /*RECOGIDO DE LA BBDD ESTRATEGICA*/
+    private nombre:string="";
+    private apellidos:string="";
+    private nacionalidad:string="";
+    private profesion:string="";
+    private biografia:string="";
+    private imagen:string="";
+    constructor(nombre:string, apellidos:string, nacionalidad:string, profesion:string, biografia:string, imagen:string){
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.nacionalidad=nacionalidad;
+        this.profesion=profesion;
+        this.biografia=biografia;
+        this.imagen=imagen;
+    }
+    public getNombre():string
+    {
+      return this.nombre;
+    }
+    public getApellidos():string
+    {
+      return this.apellidos;
+    }
+    public getNacionalidad():string
+    {
+      return this.nacionalidad;
+    }
+    public getProfesion():string
+    {
+      return this.profesion;
+    }
+    public getBiografia():string
+    {
+      return this.biografia;
+    }
+    public getImagen():string
+    {
+      return this.imagen;
+    }
+    public setNombre(nombre:string):void
+    {
+      this.nombre=nombre;
+    }
+    public setApellidos(apellidos:string):void
+    {
+      this.apellidos=apellidos;
+    }
+    public setNacionalidad(nacionalidad:string):void
+    {
+      this.nacionalidad=nacionalidad;
+    } 
+    public setProfesion(profesion:string):void
+    {
+      this.profesion=profesion;
+    } 
+    public setBiografia(biografia:string):void
+    {
+      this.biografia=biografia;
+    }
+    public setImagen(imagen:string):void
+    {
+      this.imagen=imagen;
     }
 }
