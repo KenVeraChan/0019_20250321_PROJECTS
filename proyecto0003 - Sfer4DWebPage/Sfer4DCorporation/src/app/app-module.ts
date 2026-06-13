08d9eed52bc5dbb94d11d67881bcfa8fe25ee22b
KenVeraChan <kenveracha@gmail.com>
2026-06-13 17:51:49 +0200

    Primer commit del proyecto Master conversion PHP a Angular

---
+++ proyecto0003 - Sfer4DWebPage/Sfer4DCorporation/src/app/app-module.ts
@@ -0,0 +1,14 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Cookie } from './cookie/cookie';
+
+@NgModule({
+  declarations: [App, Cookie],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
+  bootstrap: [App],
+})
+export class AppModule {}
---
+++ proyecto0003 - Sfer4DWebPage/Sfer4DCorporation/src/app/cookie/cookie.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie',
+  standalone: false,
+  templateUrl: './cookie.html',
+  styleUrl: './cookie.css',
+})
+export class Cookie {}
