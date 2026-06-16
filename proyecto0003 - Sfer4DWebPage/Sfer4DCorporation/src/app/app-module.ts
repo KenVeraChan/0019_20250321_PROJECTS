c9aaa00ee2fd31f1fa5456e1dd0bcb399d2cb5e3
KenVeraChan <kenveracha@gmail.com>
2026-06-16 15:36:34 +0200

    generando los componentes de la pagina web

--- proyecto0003 - Sfer4DWebPage/Sfer4DCorporation/src/app/app-module.ts
+++ proyecto0003 - Sfer4DWebPage/Sfer4DCorporation/src/app/app-module.ts
@@ -4,9 +4,28 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Cookie } from './cookie/cookie';
+import { Inicio } from './inicio/inicio';
+import { Historia } from './historia/historia';
+import { Productos } from './productos/productos';
+import { Servicios } from './servicios/servicios';
+import { Proyectos } from './proyectos/proyectos';
+import { Cliente } from './cliente/cliente';
+import { S4DEControlVentasInterfaz } from './s4-de-control-ventas-interfaz/s4-de-control-ventas-interfaz';
+import { S4dControlVentasInterfaz } from './s4d-control-ventas-interfaz/s4d-control-ventas-interfaz';
 
 @NgModule({
-  declarations: [App, Cookie],
+  declarations: [
+    App,
+    Cookie,
+    Inicio,
+    Historia,
+    Productos,
+    Servicios,
+    Proyectos,
+    Cliente,
+    S4DEControlVentasInterfaz,
+    S4dControlVentasInterfaz,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
   bootstrap: [App],
---
+++ proyecto0003 - Sfer4DWebPage/Sfer4DCorporation/src/app/s4d-control-ventas-interfaz/s4d-control-ventas-interfaz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-s4d-control-ventas-interfaz',
+  standalone: false,
+  templateUrl: './s4d-control-ventas-interfaz.html',
+  styleUrl: './s4d-control-ventas-interfaz.css',
+})
+export class S4dControlVentasInterfaz {}
