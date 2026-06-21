adf1444a42623d6cb2a78033fed685c617882661
KenVeraChan <kenveracha@gmail.com>
2026-06-22 00:00:26 +0200

    Mejoras I en las fuentes de las paginas

--- proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/pie/pie.ts
+++ proyecto0002- EscritoresSinFronteras/paginaDefinitivaESF/src/app/pie/pie.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Pie {
 
+private enlaceGitHub: string = 'https://github.com/KenVeraChan';
+  public abrirGitHub(): void 
+  {
+    window.open(this.enlaceGitHub, '_blank');
+  }
+
 }
